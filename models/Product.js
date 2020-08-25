120bbe248b10bfe603c99f534dc78190a0a69ff4
justadigit <letmecode123456@gmail.com>
2020-08-25 10:39:33 +0630

    Modified

--- models/Product.js
+++ models/Product.js
@@ -1,33 +1,36 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const productSchema = new Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  price: {
-    type: Number,
-    required: true,
-  },
-  image: {
-    type: String,
-    required: true,
-  },
-  specification: {
-    type: String,
-    required: true,
-  },
-  relatedImage: [
-    {
+const productSchema = new Schema(
+  {
+    name: {
       type: String,
-      requried: true,
+      required: true,
     },
-  ],
-  categoryId: {
-    type: Schema.Types.ObjectId,
-    ref: 'Category',
-    required: true,
+    price: {
+      type: Number,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    specification: {
+      type: String,
+      required: true,
+    },
+    relatedImage: [
+      {
+        type: String,
+        requried: true,
+      },
+    ],
+    categoryId: {
+      type: Schema.Types.ObjectId,
+      ref: 'Category',
+      required: true,
+    },
   },
-});
+  { timestamps: true }
+);
 module.exports = mongoose.model('Product', productSchema);
